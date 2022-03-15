f905b6ff9752f170b0e11557c19b03a2063d2121
Badi1298 <serbandavid83@yahoo.com>
2022-03-15 18:17:32 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9b82812e-9fdd-4d28-8bf6-22b0fc1907fb';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/searchView.js
@@ -0,0 +1,24 @@
+class SearchView {
+  _parentEl = document.querySelector('.search');
+
+  getQuery() {
+    const query = this._parentEl.querySelector('.search__field').value;
+    this._clearInput();
+    return query;
+  }
+
+  _clearInput() {
+    const searchField = this._parentEl.querySelector('.search__field');
+    searchField.value = '';
+    searchField.blur();
+  }
+
+  addHandlerSearch(handler) {
+    this._parentEl.addEventListener('submit', function (e) {
+      e.preventDefault();
+      handler();
+    });
+  }
+}
+
+export default new SearchView();
